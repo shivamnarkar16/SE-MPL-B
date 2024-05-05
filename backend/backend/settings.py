d6be384fe3a42868e4675b92e381dcb165b612c7
Shivam Narkar <shivam.narkar@vpt.edu.in>
2024-05-05 17:06:29 +0530

    chore: Remove corsheaders from INSTALLED_APPS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "corsheaders",
+    # "corsheaders",
     "app",
     "rest_framework_simplejwt.token_blacklist",
 ]
