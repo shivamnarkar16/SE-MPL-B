f8f8a79e9931a82ccc208eaabf4673c7face9301
Shivam Narkar <shivam.narkar@vpt.edu.in>
2024-05-05 19:47:59 +0530

    chore: Update RAZORPAY_KEY_ID and RAZORPAY_KEY_SECRET

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -174,5 +174,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # Razorpay
-RAZORPAY_KEY_ID = os.environ.get("RAZORPAY_KEY_ID")
-RAZORPAY_KEY_SECRET = os.environ.get("RAZORPAY_KEY_SECRET")
+RAZORPAY_KEY_ID = "rzp_test_ayUd0ehWxUbVZE"
+RAZORPAY_KEY_SECRET = "zuEvbekUoMUKAk89Zw3wqZVG"
